8255dfab2596bc88dd0fe98c36ff39e7e765b62a
KowalDrzo <248959@student.pwr.edu.pl>
2021-08-06 20:59:20 +0200

    Dopisanie instrukcji ramki wysyłanej

--- ATM_Separ/Globals.h
+++ ATM_Separ/Globals.h
@@ -3,14 +3,14 @@
  * Kolejność:
  * MSB -> 76543210 <- LSB:
  * 
- * 0.
- * 1.
- * 2.
- * 3.
- * 4.
- * 5.
- * 6.
- * 7.
+ * 0. Czy Atmega jest uzbrojona softowo? 0 - nie, 1 - tak,
+ * 1. Czy zrobiono separację 1 stopnia?
+ * 2. Czy zrobiono separację 2 stopnia?
+ * 3. Czy Altimax wykrył apogeum?
+ * 4. Czy Altimax wykrył 200m na ziemią?
+ * 5. Czy telemetrum wykrył apogeum?
+ * 6. Czy telemetrum wykrył 200m nad ziemią?
+ * 7. Kontrola parzystości - jeśli w poprzednich bitach jest parzysta liczba jedynek, to tutaj ma być 1.
  */
 
 uint8_t SeparationFrame = 0;
