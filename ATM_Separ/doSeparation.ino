c08f209fe137ac7bacd055775a081a4960fbb9cc
KowalDrzo <248959@student.pwr.edu.pl>
2021-08-07 08:58:08 +0200

    Poprawka mała

--- ATM_Separ/doSeparation.ino
+++ ATM_Separ/doSeparation.ino
@@ -23,21 +23,18 @@ void doSecondSeparation() {
   // Zapalniki podstawowe:
   PORTB |= SEPAR2A_Pin;
 
-  uint32_t timer_ms = millis();
-  while(millis() - timer_ms < 2000) {
+  _delay_ms(2000);
 
-    if(PIND & TEST2_Pin) {
+  if(PIND & TEST2_Pin) {
 
-      PORTB &= ~SEPAR2A_Pin;
-      return; // Wyjście z funkcji, jeśli separacja uda się przy zapalnikach podstawowych
-    }
+    PORTB &= ~SEPAR2A_Pin;
+    return; // Wyjście z funkcji, jeśli separacja uda się przy zapalnikach podstawowych
   }
 
   // Zapalniki awaryjne:
   PORTB |= SEPAR2B_Pin;
   
-  timer_ms = millis();
-  while(millis() - timer_ms < 2000) {;}
+  _delay_ms(2000);
   
   PORTB &= ~SEPAR2A_Pin;
   PORTB &= ~SEPAR2B_Pin;
