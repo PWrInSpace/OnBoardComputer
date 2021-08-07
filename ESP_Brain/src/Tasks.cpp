b1c68710107f077b7fbafe2404be8e51b464c308
KowalDrzo <248959@student.pwr.edu.pl>
2021-08-07 08:57:04 +0200

    Ostatnie poprawki odzyskowego i2c

--- ESP_Brain/src/Tasks.cpp
+++ ESP_Brain/src/Tasks.cpp
@@ -1,5 +1,7 @@
 #include "Tasks.h"
 
+// Zadanie zajmujące się wszystkim, co łączy się przez i2c:
+
 void i2cTask(void *arg) {
 
     Wire.begin();
@@ -9,28 +11,28 @@ void i2cTask(void *arg) {
         for (int i = 0; i < 5; i++) {
         
             Wire.requestFrom(3, 1);
-            delay(10);
+            vTaskDelay(10 / portTICK_PERIOD_MS);
             if (Wire.available()) {
                 int val = Wire.read();
-                Serial.println(val);
+                Serial.println(val); // Tylko do debugu
             }
-            delay(500);
+            vTaskDelay(500 / portTICK_PERIOD_MS);
         }
 
         Wire.beginTransmission(3);
         Wire.write(8);
         Wire.endTransmission();
-        delay(5);
+        vTaskDelay(5 / portTICK_PERIOD_MS);
 
         for (;;) {
             
             Wire.requestFrom(3, 1);
-            delay(10);
+            vTaskDelay(10 / portTICK_PERIOD_MS);
             if (Wire.available()) {
                 int val = Wire.read();
-                Serial.println(val);
+                Serial.println(val); // Tylko do debugu
             }
-            delay(500);
+            vTaskDelay(500 / portTICK_PERIOD_MS);
         }
     }
 }
