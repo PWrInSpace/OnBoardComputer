9feb87e1512ce092f962cdbb3113f832f03239fa
KowalDrzo <248959@student.pwr.edu.pl>
2021-09-22 21:30:05 +0200

    Poprawiono wyliczaną wartość - już powinno być ok

--- ESP_Brain/src/LoopTasks.cpp
+++ ESP_Brain/src/LoopTasks.cpp
@@ -186,9 +186,10 @@ void adcTask(void *arg) {
     while(1) {
 
         // Ciśnienie butli:
-        mainDataFrame.tankPressure = analogRead(GPIO_NUM_34) * 0.06105; // * 250 / 4095
+        mainDataFrame.tankPressure = analogRead(GPIO_NUM_34) * 0.0201465; // * 82.5 / 4095
         /* 
-         * Mnożymy przez 250, bo do 250 bar,
+         * Mnożymy przez 82.5, bo przy takim ciśnieniu będzie maksymalne napięcie na adc - 3.3v
+         * (Czujnik daje 10v przy 250 barach).
          * dzielimy przez 4095 bo to max wartość z przetwornika
          */
 
