e65842c9e8b40bc1fb47c0c5da00452466a3e242
Michał Kowalski <248959@student.pwr.edu.pl>
2021-09-23 21:07:54 +0200

    Poprawa lory

--- STM_Round/Core/Src/LoraRMF.c
+++ STM_Round/Core/Src/LoraRMF.c
@@ -15,7 +15,7 @@ void loraInit(void) {
 
 	SX1278_begin(&SX1278, 868000000, SX1278_POWER_20DBM, SX1278_LORA_SF_7, SX1278_LORA_BW_125KHZ, 10);
 
-	SX1278_LoRaEntryRx(&SX1278, BUFFER_SIZE, 2000);
+	SX1278_LoRaEntryRx(&SX1278, BUFFER_SIZE, LORA_TIMEOUT);
 
 }
 
@@ -23,12 +23,12 @@ void loraInit(void) {
 
 void loraSendData(uint8_t *data, int len) {
 
-	loraRet = SX1278_LoRaEntryTx(&SX1278, len, 2000);
+	loraRet = SX1278_LoRaEntryTx(&SX1278, len, LORA_TIMEOUT);
 
-	loraRet = SX1278_LoRaTxPacket(&SX1278, data, len, 2000);
+	loraRet = SX1278_LoRaTxPacket(&SX1278, data, len, LORA_TIMEOUT);
 
 
-	SX1278_LoRaEntryRx(&SX1278, BUFFER_SIZE, 2000);
+	SX1278_LoRaEntryRx(&SX1278, BUFFER_SIZE, LORA_TIMEOUT);
 }
 
 /****************************************************************************************/
