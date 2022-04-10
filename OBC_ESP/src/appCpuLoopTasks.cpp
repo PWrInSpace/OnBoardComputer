f98136b7c87bb95639a8a4af634ef35283b86def
Michał Kowalski <248959@student.pwr.edu.pl>
2022-04-10 11:25:23 +0200

    Tymczasowy fix I2C

--- OBC_ESP/src/appCpuLoopTasks.cpp
+++ OBC_ESP/src/appCpuLoopTasks.cpp
@@ -204,13 +204,14 @@ void dataTask(void *arg){
       // TODO!!!
 
       // Recovery:
-      /*
-      rc.i2c1.requestFrom(RECOVERY_ADDRES, sizeof(RecoveryData));
-      if (rc.i2c1.available()) {
-        if (!rc.i2c1.readBytes((uint8_t*) &dataFrame.recovery, sizeof(RecoveryData))) {
+      vTaskDelay(1/ portTICK_PERIOD_MS);  
+      rc.i2c2.requestFrom(RECOVERY_ADDRES, sizeof(RecoveryData));
+      if (rc.i2c2.available()) {
+        if (!rc.i2c2.readBytes((uint8_t*) &dataFrame.recovery, sizeof(RecoveryData))) {
         // ERROR I2C
         }
-      }*/
+      }
+      vTaskDelay(1/ portTICK_PERIOD_MS);  
 
       //read i2c comm data
       //rc.sendLog("Hello space!");
