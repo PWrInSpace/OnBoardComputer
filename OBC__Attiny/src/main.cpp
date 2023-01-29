f6e64ecb37a9470bd3d0aae2a99336d5eefe2401
PawelSucharzewski <259307@student.pwr.edu.pl>
2023-01-29 12:16:53 +0100

    Servo Control

--- OBC__Attiny/src/main.cpp
+++ OBC__Attiny/src/main.cpp
@@ -1,9 +1,22 @@
 #include <Arduino.h>
+#include <avr/io.h>
+
+#define CLOSED 0x0A // Pozycja 0 deg
+#define OPEN 0x2D // Pozycja 180 deg
 
 void setup() {
-  // put your setup code here, to run once:
+
+  DDRB = 0x01;
+  PORTB = 0x02;
+  TCCR0A |= (1<<COM0A1) | (1<<WGM02) | (1<<WGM01) | (1<<WGM00);
+  TCCR0B |= (1<<CS01) | (1<<CS00);
+  OCR0A = CLOSED;   
+   
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+      if(!(PINB & 0x02)){
+        OCR0A = OPEN; 
+      }  
 }
