f29b2d2864b1eee76ded73e97bf8da02e7cd6464
PawelSucharzewski <259307@student.pwr.edu.pl>
2023-02-21 17:42:07 +0100

    added comments

--- OBC__Attiny/src/main.cpp
+++ OBC__Attiny/src/main.cpp
@@ -4,19 +4,39 @@
 #define CLOSED 0x0A // Pozycja 0 deg
 #define OPEN 0x2D // Pozycja 180 deg
 
-void setup() {
-
-  DDRB = 0x05;
-  PORTB = 0x08;
+void setTimers(){
+  /*********************************************************
+  * USTAWIENIE REJSETRÓW                                   *
+  * TCCR0A - Fast PWM, maksymalna wartośc licznika OCR0A   *
+  * TCCR0B - Prescaler ckl/64                              *
+  *   MIEJSCE NA OBLICZENIA !!!!                           *
+  **********************************************************/
   TCCR0A |= (1<<COM0A1) | (1<<WGM02) | (1<<WGM01) | (1<<WGM00);
   TCCR0B |= (1<<CS01) | (1<<CS00);
-  OCR0A = CLOSED;   
+  OCR0A = CLOSED; 
+}
+
+void setup() {
+
+  DDRB = 0x05;  // PORTY PB0 I PB2 USTAWIANE JAKO OUTPUT
+  PORTB = 0x08; // PORT PB3 USTAWIONY JAKO INPUT_PULLUP, PB4 JAKO INPUT
+  setTimers();
    
 }
 
 void loop() {
+
+       /*******************************************************
+       * (PINB & 0x10) -> GDY NA PB4 POJAWI SIE STAN WYSOKI   *
+       *                  LUB                                 *
+       * !(PINB & 0x08) -> STAN NASKI NA PB3                  *
+       ********************************************************/ 
+
       if((PINB & 0x10) | !(PINB & 0x08)){
-        OCR0A = OPEN; 
-        PORTB |= (1<<PB2);
+        
+        OCR0A = OPEN; // ZMIANA WARTOSCI LICZNIKA, ZMINIA WYPELNIENIE PWM
+        
+        PORTB |= (1<<PB2); // STAN PB2 USTAWIANY JAKO WYSOKI
+      
       }  
 }
